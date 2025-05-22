7976fcbf1de10cf72dc04bcd87d3b8fade683256
neinron <jaron@schurer.net>
2025-05-22 23:39:40 +0200

    On branch improve-subscription Your branch is up to date with 'origin/improve-subscription'.
    Changes to be committed:
    	modified:   next.config.js

--- next.config.js
+++ next.config.js
@@ -19,6 +19,10 @@ const nextConfig = {
             key: 'Access-Control-Allow-Headers',
             value: 'Content-Type, Authorization',
           },
+          {
+            key: 'Access-Control-Allow-Origin',
+            value: 'https://reminder.wemolo.com',
+          },
         ],
       },
     ];
